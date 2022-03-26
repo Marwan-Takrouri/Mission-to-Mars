e786dd2dec8a25ba7fdd50f4d1970d84d430bb09
Marwan-Takrouri <monitak2000@gmail.com>
2022-03-26 14:04:56 -0400

    commit

--- challenege code files/scraping.py
+++ challenege code files/scraping.py
@@ -1,28 +1,10 @@
-
-##SCRAPE ALL
-def scrape_all():
-    # Initiate headless driver for deployment
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
-    news_title, news_paragraph = mars_news(browser)
-
-# Run all scraping functions and store results in dictionary
-data = {
-      "news_title": news_title,
-      "news_paragraph": news_paragraph,
-      "featured_image": featured_image(browser),
-      "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
-}
-# Stop webdriver and return data
-   browser.quit()
-   #return data
+import pandas as pd
 
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
+
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
@@ -31,7 +13,7 @@ url = 'https://redplanetscience.com'
 browser.visit(url)
 
 
-##def mars_news():
+def mars_news():
 def mars_news(browser):
 
    # Visit the mars nasa news site
@@ -118,7 +100,7 @@ def mars_news(browser):
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
     except AttributeError:
-    return None, None
+        return None, None
 
     return news_title, news_p
 
@@ -173,9 +155,36 @@ def mars_facts():
     return df.to_html()
 
 
+ #scrape the hemisphere data :
+
+
+url = 'https://marshemispheres.com/'
+
+browser.visit(url)
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+number_thumb = len(browser.find_by_css('a.product-item img'))
+number_thumb
+
+
+   for i in range(number_thumb):
+    browser.find_by_css('a.product-item img')[i].click()
+    hemisphere = {}
+    hemisphere['title'] = browser.find_by_css('h2.title').first.text
+    hemisphere['image'] = browser.find_by_text('Sample').first['href']
+    hemisphere_image_urls.append(hemisphere)
+    browser.back()
+
+# 4. Print the list that holds the dictionary of each image url and title.
+   return hemisphere_image_urls
 
 
 
+if __name == "__main__" :
+ print (scape_all ())
 
 
 
